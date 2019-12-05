e4da5e08d38b8c32d9e0d85ef10f1e868bb358e1
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-12-05 14:39:35 +0900

    rm

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Exposer, ExposerObject, Wrapper, expose, wrap } from "./main";
-import { workerThreadsExposer, workerThreadsWrapper } from "./worker";
-export { Wrapper, Exposer, ExposerObject, wrap, expose, workerThreadsExposer, workerThreadsWrapper };
--- lib/worker.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import { Exposer, Wrapper } from "./main";
-import { Worker } from "worker_threads";
-export declare const workerThreadsWrapper: (worker: Worker) => Wrapper;
-export declare const workerThreadsExposer: () => Exposer;
