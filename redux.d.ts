8f771a0e820c453e47a71ee4c2babb3ec4044df2
shinyoshiaki <shinyoshiaki@users.noreply.github.com>
2019-12-06 07:42:20 +0000

    Automated deployment: Fri Dec  6 07:42:20 UTC 2019 5fb9a037bc5e36ffc52f59329de8dc405bc4db1a

--- redux.d.ts
+++ redux.d.ts
@@ -2,4 +2,4 @@ export declare type WrapperSync = (v: any) => void;
 export declare function wrapRedux<T>(target: {
     new (...args: any[]): T;
 }, wrapper: WrapperSync): T;
-export declare function exposeRedux(instance: any): (state: any, v: any) => any;
+export declare function exposeRedux<T extends any>(instance: T): (state: any, v: any) => T["state"];
