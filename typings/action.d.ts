16860bfcbe3dafe6f0e993e08112bc2c2706607a
shinyoshiaki <shinyoshiaki@users.noreply.github.com>
2019-12-06 17:52:17 +0000

    Automated deployment: Fri Dec  6 17:52:17 UTC 2019 a6d18b1fe1e71518ee17b61e15f909e380f1a0e5

--- typings/action.d.ts
+++ typings/action.d.ts
@@ -1,6 +1,8 @@
 export declare type Action<T, B> = {
-    [K in keyof T]: T[K] extends (...args: infer U) => B ? (...args: U) => {
-        type: string;
-        args: U;
-    } : never;
+    [K in keyof T]: T[K] extends (...args: infer U) => infer R ? Include<R, B, U> : never;
 };
+declare type Include<A, B, U extends any[]> = B extends A ? (...args: U) => {
+    type: string;
+    args: U;
+} : never;
+export {};
