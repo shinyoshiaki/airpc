22ac5523497e72805a00731744fd5b978fb83dec
shinyoshiaki <shinyoshiaki@users.noreply.github.com>
2019-12-07 02:48:41 +0000

    Automated deployment: Sat Dec  7 02:48:41 UTC 2019 cf2b7e7a80f9de520de3e0fa7cec9da1dd6fefea

--- redux.d.ts
+++ redux.d.ts
@@ -1,4 +1,4 @@
-import { Action } from "./typings/action";
+import { ActionCreator, ValidState } from "./typings/action";
 export declare function withRedux<A extends any, B>(target: {
     new (state: B): A;
-}, initialState: B): [Omit<Action<A, B>, "state">, (state: B | undefined, action: Omit<A[keyof A], "state">) => A["state"]];
+}, initialState: B): [Omit<ActionCreator<A, B>, "state">, (state: B | undefined, action: Omit<A[keyof A], "state">) => ValidState<B, A>];
--- typings/action.d.ts
+++ typings/action.d.ts
@@ -1,8 +1,9 @@
-export declare type Action<T, B> = {
+export declare type ActionCreator<T, B> = {
     [K in keyof T]: T[K] extends (...args: infer U) => infer R ? Include<R, B, U> : never;
 };
 declare type Include<A, B, U extends any[]> = B extends A ? (...args: U) => {
     type: string;
     args: U;
 } : never;
+export declare type ValidState<A, B> = B extends A ? B : never;
 export {};
