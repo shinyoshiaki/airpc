0dd001eeb8bb241f6d1017cec6bae1b83db4ac56
shinyoshiaki <shinyoshiaki@users.noreply.github.com>
2019-12-07 03:27:43 +0000

    Automated deployment: Sat Dec  7 03:27:43 UTC 2019 6970e433674c7554ab2983597af39317f0de8085

--- redux.d.ts
+++ redux.d.ts
@@ -1,4 +1,4 @@
 import { ActionCreator, ValidState } from "./typings/action";
 export declare function withRedux<A extends any, B>(target: {
     new (state: B): A;
-}, initialState: B): [Omit<ActionCreator<A, B>, "state">, (state: B | undefined, action: Omit<A[keyof A], "state">) => ValidState<B, A>];
+}, initialState: B): [Omit<ActionCreator<A, Required<B>>, "state">, (state: B | undefined, action: Omit<A[keyof A], "state">) => ValidState<B, A>];
